091dff04a96e1be1751ce46febad9daa8ea6cc44
ItamarAlon <itamar20112@gmail.com>
2024-01-16 23:22:58 +0200

    Deleted string from shape

--- Project Tetris/gameConfig.h
+++ Project Tetris/gameConfig.h
@@ -6,7 +6,7 @@ class GameConfig
 public:
 	enum class Lkeys { LEFT = 'a', RIGHT = 'd', CLOCKWISE = 's', COUNTER_CLOCKWISE = 'w' , ESC = 27, DOWN='h'};
 	enum class Rkeys { LEFT = 'j', RIGHT = 'l', CLOCKWISE = 'k', COUNTER_CLOCKWISE = 'i', ESC = 27 };
-	static constexpr int BOARD_WIDTH = 10;
+	static constexpr int BOARD_WIDTH = 11;
 	static constexpr int BOARD_HEIGHT = 20; //Change dimentions later
 };
 
